940aa23044529e3025b452e3e9ea1793eef5077d
Beata Gabrielova <beata.gabrielova@northern.tech>
2023-06-15 13:38:00 +0200

    changed title + formatted code

--- src/App.jsx
+++ src/App.jsx
@@ -4,40 +4,41 @@ import viteLogo from '/vite.svg'
 import './App.css'
 const contentByLanguage = {
   Spanish: {
-    title:'España',textCountry:'España es un país hermoso que tiene mucho que ofrecer. Esta página estará especialmente dedicada a Lluís, uno de los mejores escaladores españoles.', flag:'https://placekitten.com/100/100'
+    title: 'España', textCountry: 'España es un país hermoso que tiene mucho que ofrecer. Esta página estará especialmente dedicada a Lluís, uno de los mejores escaladores españoles!', flag: 'https://placekitten.com/100/100'
   },
   English: {
-    title:'Spain',textCountry:'Spain is a beautiful country that has a lot to offer. This page is for Lluís, one of the best Spanish climbers.', flag:'https://placekitten.com/80/80'
-}}
+    title: 'Spain', textCountry: 'Spain is a beautiful country that has a lot to offer. This page is for Lluís, one of the best Spanish climbers.', flag: 'https://placekitten.com/80/80'
+  }
+}
 
 function App() {
   const [count, setCount] = useState(0)
   const [text, setText] = useState('vino de la casa')
   const [fontFamily, setFontFamily] = useState('Arial')
-  const [image, setImage] = useState ('https://placekitten.com/200/200')
-  const [language, setLanguage] = useState ('Spanish')
+  const [image, setImage] = useState('https://placekitten.com/200/200')
+  const [language, setLanguage] = useState('Spanish')
   const onImageClick = () => {
     setImage("https://placekitten.com/150/150")
   }
   const onClickHandler = () => {
     setText("¿Hola, cómo estás?")
     setFontFamily("Montserrat")
-    let newLanguage = 'English' 
-    if (language=='English') {
-      newLanguage='Spanish'
+    let newLanguage = 'English'
+    if (language == 'English') {
+      newLanguage = 'Spanish'
     }
 
-    setLanguage (newLanguage)
+    setLanguage(newLanguage)
   }
   return (
     <div style={{ fontFamily: "Montserrat" }}>
       <h2 onClick={onClickHandler}>{contentByLanguage[language].title}</h2>
       {contentByLanguage[language].textCountry}<br />Espero que te diviertas.
       <div style={{ fontFamily }}>
-       {text} 
+        {text}
       </div>
-      <div style={{display:'grid', placeContent: 'center'}}>
-        <img className='logo fancy' onClick={onImageClick} src = {image}/>
+      <div style={{ display: 'grid', placeContent: 'center' }}>
+        <img className='logo fancy' onClick={onImageClick} src={image} />
       </div>
     </div>
   );
