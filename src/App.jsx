4d17858e68b49bef2cb099fb37fb126f15b5935a
Beata Gabrielova <beata.gabrielova@northern.tech>
2023-11-03 14:20:41 +0100

    lines

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,14 @@ import Gallery from './gallery'
 
 const contentByLanguage = {
   English: {
-    title: 'Welcome to My Art Jungle', textCountry: 'Here, you will find the colorful chaos of an amateur artist letting her imagination run wild. Enjoy the gallery of accidental masterpieces, where imperfection is my signature style.', flag: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Flag_of_Norway.svg/512px-Flag_of_Norway.svg.png', style: "text-align: center;", things: [
+    title: 'Welcome to My Art Jungle', textCountry: ['Here, you will find the colorful chaos of an amateur artist letting her imagination run wild.', 'Enjoy the gallery of accidental masterpieces, where imperfection is my signature style.'], flag: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Flag_of_Norway.svg/512px-Flag_of_Norway.svg.png', things: [
       { title: 'Večerní', image: Vecerni, width: 2714, height: 3471 },
       { title: 'Vášnivá', image: Vasniva, width: 2678, height: 3428 },
       { title: 'Beznadějná', image: Beznadejna, width: 2807, height: 3547 }
     ]
   },
   Norwegian: {
-    title: 'Velkommen til kunstjungelen min', textCountry: 'Her finner du det fargerike kaoset til en amatørkunstner som lar fantasien løpe løpsk. Nyt galleriet med tilfeldige mesterverk, der ufullkommenhet er min signaturstil.', flag: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Flag_of_the_United_Kingdom_%281-2%29.svg/1200px-Flag_of_the_United_Kingdom_%281-2%29.svg.png', things: [
+    title: 'Velkommen til kunstjungelen min', textCountry: ['Her finner du det fargerike kaoset til en amatørkunstner som lar fantasien løpe løpsk.', 'Nyt galleriet med tilfeldige mesterverk, der ufullkommenhet er min signaturstil.'], flag: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Flag_of_the_United_Kingdom_%281-2%29.svg/1200px-Flag_of_the_United_Kingdom_%281-2%29.svg.png', things: [
       { title: 'Večerní', image: Vecerni, width: 2714, height: 3471 },
       { title: 'Vášnivá', image: Vasniva, width: 2678, height: 3428 },
       { title: 'Beznadějná', image: Beznadejna, width: 2807, height: 3547 }
@@ -26,12 +26,12 @@ function App() {
   const [text, setText] = useState('Cheers!')
   const [customFontFamily, setCustomFontFamily] = useState('Courier New')
   const [image, setImage] = useState('https://placekitten.com/200/200')
-  const [language, setLanguage] = useState(Object.keys(contentByLanguage)[0]);
+  const [language, setLanguage] = useState('English')
   const [count, setCount] = useState(0)
   const onFlagClick = () => {
-    let newLanguage = Object.keys(contentByLanguage)[1];
-    if (language == newLanguage) {
-      newLanguage = Object.keys(contentByLanguage)[0];
+    let newLanguage = 'Norwegian'
+    if (language == 'Norwegian') {
+      newLanguage = 'English'
     }
     setLanguage(newLanguage)
     setCount(count + 1)
@@ -45,9 +45,11 @@ function App() {
 
   }
   return (
-    <div style={{ fontFamily: "Courier New" }}>
+    <div style={{ textAlign: 'center' }}>
       <h1 onClick={onClickHandler}>{contentByLanguage[language].title} </h1>
-      {contentByLanguage[language].textCountry}<br />
+      {contentByLanguage[language].textCountry.map((line) =>
+        <div> {line} </div>
+      )}<br />
       <div style={{ fontFamily: customFontFamily }}>
         {text}
       </div>
